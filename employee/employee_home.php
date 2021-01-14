b9708109f13e0e35a7ddbf0c17d8aaad87fc8e0a
zannatul225 <zannatulferdous489@gmail.com>
2021-01-15 01:53:16 +0600

    .

--- employee/employee_home.php
+++ employee/employee_home.php
@@ -94,43 +94,6 @@ if(isset($_SESSION['eid'])){
                border: 1px solid #ccc;
              }
            }
-
-          /* Style the tab */
-          .tab {
-            overflow: hidden;
-            border: 1px solid #ccc;
-            background-color: #f1f1f1;
-          }
-
-          /* Style the buttons inside the tab */
-          .tab button {
-            background-color: inherit;
-            float: left;
-            border: none;
-            outline: none;
-            cursor: pointer;
-            padding: 14px 16px;
-            transition: 0.3s;
-            font-size: 17px;
-          }
-
-          /* Change background color of buttons on hover */
-          .tab button:hover {
-            background-color: #ddd;
-          }
-
-          /* Create an active/current tablink class */
-          .tab button.active {
-            background-color: #ccc;
-          }
-
-          /* Style the tab content */
-          .tabcontent {
-            display: none;
-            padding: 6px 12px;
-            border: 1px solid #ccc;
-            border-top: none;
-          }
        </style>
    </head>
    <body>
@@ -158,27 +121,5 @@ if(isset($_SESSION['eid'])){
          <p>Navigation bar with a search box and a submit button inside of it.</p>
          <p>Resize the browser window to see the responsive effect.</p>
        </div>
-
-       <div class="tab">
-        <button class="tablinks" onclick="openCity(event, 'London')">London</button>
-        <button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
-        <button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button>
-      </div>
-
-      <div id="London" class="tabcontent">
-        <h3>London</h3>
-        <p>London is the capital city of England.</p>
-      </div>
-
-      <div id="Paris" class="tabcontent">
-        <h3>Paris</h3>
-        <p>Paris is the capital of France.</p>
-      </div>
-
-      <div id="Tokyo" class="tabcontent">
-        <h3>Tokyo</h3>
-        <p>Tokyo is the capital of Japan.</p>
-      </div>
-
    </body>
  </html>
