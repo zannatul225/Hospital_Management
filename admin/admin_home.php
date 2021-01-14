dbc5260d2ce14733aa0a06912745f73bf87e6f5b
zannatul225 <zannatulferdous489@gmail.com>
2021-01-15 04:22:54 +0600

    admin nav

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -15,32 +15,51 @@ $res = mysqli_query($db,$sql);
 $check = mysqli_num_rows($res);
 
 ?>
+
 <!DOCTYPE html>
+<!-- Created By CodingNepal -->
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Admin Home</title>
-    <link rel="stylesheet" href="admin_insert.css" type="text/css">
+    <!-- Somehow I got an error, so I comment the title, just uncomment to show -->
+    <!-- <title>Sider Menu Bar CSS</title> -->
+    <link rel="stylesheet" href="admin.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
   </head>
   <body>
-    <center><h1>Care and Cure Hospital</h1></center>
+    <input type="checkbox" id="check">
+    <label for="check">
+      <i class="fas fa-bars" id="btn"></i>
+      <i class="fas fa-times" id="cancel"></i>
+    </label>
+    <div class="sidebar">
+    <header>My App</header>
+    <ul>
+      <li><a href="#"><i class="fas fa-qrcode"></i>Dashboard</a></li>
+      <li><a href="#"><i class="fas fa-link"></i>Shortcuts</a></li>
+      <li><a href="#"><i class="fas fa-stream"></i>Overview</a></li>
+      <li><a href="#"><i class="fas fa-calendar-week"></i>Events</a></li>
+      <li><a href="#"><i class="far fa-question-circle"></i>About</a></li>
+      <li><a href="#"><i class="fas fa-sliders-h"></i>Services</a></li>
+      <li><a href="#"><i class="far fa-envelope"></i>Contact</a></li>
+    </ul>
+    </div>
+  <section></section>
 
-    <?php
+  <center><h1>Care and Cure Hospital</h1></center>
 
-    while($row = mysqli_fetch_array($res)) {
-        echo  $row['name'] . "<br>";
-        echo "0" . $row['phone'] . "<br>";
-        echo $row['email'];
+  <?php
+  while($row = mysqli_fetch_array($res)) {
+      echo  $row['name'] . "<br>";
+      echo "0" . $row['phone'] . "<br>";
+      echo $row['email'];
+  }
+  ?>
 
-    }
-    ?>
+  <!--View room information-->
 
-      <!--View room information-->
-
-      <div1 class="button">
-         <a href="../room_details.php" class="login">View Room</a>
-      </div1>
-
-
+  <div1 class="button">
+     <a href="../room_details.php" class="login">View Room</a>
+  </div1>
   </body>
 </html>
--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$database = "hospital";
+$host = "localhost"; ///sql205.epizy.com
+$user = "root"; ///epiz_27690744
+$pass = ""; ///7OhiipJ3EelJ
+$database = "hospital"; ///epiz_27690744_hospital
 
 $db = mysqli_connect($host, $user, $pass, $database);
 ?>
